ef92513e3a59602fea06fb7d3e1d4fed037b7a08
joao.gustavo <joao.gustavofs@gmail.com>
2022-07-21 17:42:05 -0300

    Finished

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const squareAndSortArrayNumbers = (numbers) => {
+    let justNumbers = numbers.filter((n) => !isNaN(n))
+
     let array = []
     let firstIndex = 0
-    let lastIndex = numbers.length - 1
+    let lastIndex = justNumbers.length - 1
     let position = lastIndex
 
     while (firstIndex <= lastIndex) {
-        //if squared number e is greater than last value
-        if (Math.pow(numbers[firstIndex], 2) > Math.pow(numbers[lastIndex], 2)) {
-            array[position--] = Math.pow(numbers[firstIndex++], 2)
+        //if squared number e is greater than last
+        if (Math.pow(justNumbers[firstIndex], 2) > Math.pow(justNumbers[lastIndex], 2)) {
+            array[position--] = Math.pow(justNumbers[firstIndex++], 2)
             continue;
         }
-        array[position--] = Math.pow(numbers[lastIndex--], 2)
+        array[position--] = Math.pow(justNumbers[lastIndex--], 2)
     }
 
     return array
--- index.test.js
+++ index.test.js
@@ -3,15 +3,28 @@ const {squareAndSortArrayNumbers} = require('./index')
 describe('Squaring and sorting arrays', () => {
 
     it('Should square and sort just positive numbers', () => {
-        expect(squareAndSortArrayNumbers([1, 2, 3, 5, 6, 8, 9])).toStrictEqual([1, 4, 9, 25, 36, 64, 81]);
+        const result = squareAndSortArrayNumbers(["1", 2, 3, 5, 6, 8, 9, "aa"])
+        console.log("Test case 1 Sorted", result)
+        expect(result).toStrictEqual([1, 4, 9, 25, 36, 64, 81]);
     });
 
     it('Should square and sort just negative numbers', () => {
-        expect(squareAndSortArrayNumbers([-2, -1])).toStrictEqual([1, 4]);
+        const result = squareAndSortArrayNumbers([-2, -1])
+        console.log("Test case 2 Sorted", result)
+        expect(result).toStrictEqual([1, 4]);
     });
 
     it('Should square and sort positive and negative numbers', () => {
-        expect(squareAndSortArrayNumbers([-10, -5, 0, 5, 10])).toStrictEqual([0, 25, 25, 100, 100]);
+        const result = squareAndSortArrayNumbers([-10, -5, 0, 5, 10]);
+        console.log("Test case 3 Sorted", result)
+        expect(result).toStrictEqual([0, 25, 25, 100, 100]);
     });
+
+    it('Should square and sort just numbers avoiding letters and others', () => {
+        const result = squareAndSortArrayNumbers(["1", 2, 3, 5, 6, 8, 9, "aa", "@"])
+        console.log("Test case 4 Sorted", result)
+        expect(result).toStrictEqual([1, 4, 9, 25, 36, 64, 81]);
+    });
+
 
 })
